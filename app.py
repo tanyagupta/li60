2a30819f5f43624875accaf2209f870672446491
tanyagupta <gupta.tanya@gmail.com>
2026-09-19 20:33:19 -0400

    Refresh website and add Leaf support

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/test')
-def hello():
-    return render_template('test.html')
-
-
 @app.route("/")
 def hello_world():
     return render_template('index.html')
@@ -16,6 +11,16 @@ def hello_world():
 @app.route('/tos')
 def flash_cards():
     return render_template('tos.html')
+
+
+@app.route('/privacy')
+def privacy():
+    return render_template('tos.html')
+
+
+@app.route('/leaf/privacy')
+def leaf_privacy():
+    return render_template('leaf_privacy.html')
 
 
 @app.route('/helpnewsfinder')
@@ -31,6 +36,7 @@ def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
